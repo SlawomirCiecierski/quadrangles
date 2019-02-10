aaea4ce69d651e426a41064ebdd042d8c6c92e83
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-10 16:37:22 +0100

    Automatyzacja zmian

---
+++ src/pl/ciecierski/javaconsole/Main.java
@@ -0,0 +1,13 @@
+package pl.ciecierski.javaconsole;
+/* class cieCountingQuadrangle
+method: public float countSquare (float a)
+
+
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/pl/ciecierski/javaconsole/cieCountingQuadrangles.java
@@ -0,0 +1,10 @@
+package pl.ciecierski.javaconsole;
+
+public class cieCountingQuadrangles {
+
+    public float countSquare (float a)
+    {
+        return a * a;
+    }
+
+}
