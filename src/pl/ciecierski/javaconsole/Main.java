3a3cd965888b60c213b14dcaeb9ceaa799ff607c
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-11 18:59:16 +0100

    Implemented inherited classes. Implemented Main.java.

--- src/pl/ciecierski/javaconsole/Main.java
+++ src/pl/ciecierski/javaconsole/Main.java
@@ -1,6 +1,11 @@
 package pl.ciecierski.javaconsole;
-/* class cieCountingQuadrangle
-method: public float countSquare (float a)
+/* class cieCalculateQuadrangle
+objects
+myCalculateSquare (float a)
+myCalculateRectangle (float a, float b)
+myCalulateDiamonds (float a, float h)
+myCalculateTrapeze (float a, float b, float h)
+
 
 
 */
@@ -11,8 +16,8 @@ public class Main {
 	// write your code here
         cieCalculateSquare myCalculateSquare=new cieCalculateSquare(5);
         System.out.println(myCalculateSquare.field);
-        cieCalculateRectangle myCalculateTRectangle=new cieCalculateRectangle(63,7);
-        System.out.println(myCalculateTRectangle.field);
+        cieCalculateRectangle myCalculateRectangle=new cieCalculateRectangle(63,7);
+        System.out.println(myCalculateRectangle.field);
         cieCalculateDiamonds myCalculateDiamonds=new cieCalculateDiamonds(8,9);
         System.out.println(myCalculateDiamonds.field);
         cieCalculateTrapeze myCalculateTrapeze=new cieCalculateTrapeze(44,7,2);
