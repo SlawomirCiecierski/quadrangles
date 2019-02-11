4b91562a2503f311a311c3ede415e1dc3be6f4dd
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-11 19:15:27 +0100

    Implementation Main.java.

--- src/pl/ciecierski/javaconsole/Main.java
+++ src/pl/ciecierski/javaconsole/Main.java
@@ -14,12 +14,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        cieCalculateSquare myCalculateSquare=new cieCalculateSquare(5);
+            //here will be User Interface
+
+            //calculating
+                cieCalculateSquare myCalculateSquare=new cieCalculateSquare(5);
         System.out.println(myCalculateSquare.field);
+
         cieCalculateRectangle myCalculateRectangle=new cieCalculateRectangle(63,7);
         System.out.println(myCalculateRectangle.field);
+
         cieCalculateDiamonds myCalculateDiamonds=new cieCalculateDiamonds(8,9);
         System.out.println(myCalculateDiamonds.field);
+
         cieCalculateTrapeze myCalculateTrapeze=new cieCalculateTrapeze(44,7,2);
         System.out.println(myCalculateTrapeze.field);
     }
